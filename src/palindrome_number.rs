8673337a9b5b15cdf4ab50e08bb011721622dad7
Igor Mamaev <igor.mamaev1@gmail.com>
2026-01-07 23:53:46 +0300

    Palindrome number

---
+++ src/palindrome_number.rs
@@ -0,0 +1,34 @@
+pub fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+        return false;
+    }
+    let s = x.to_string();
+    let bytes = s.as_bytes();
+    let len = bytes.len();
+    for i in 0..len / 2 {
+        if bytes[i] != bytes[len - 1 - i] {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_palindrome() {
+        let test_cases = [
+            (121, true),
+            (-121, false),
+            (10, false),
+            (12321, true),
+            (0, true),
+        ];
+
+        for (input, expected) in test_cases {
+            assert_eq!(is_palindrome(input), expected);
+        }
+    }
+}
