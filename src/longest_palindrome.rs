2942db4d79009f9d573d7c920ffc980e600d1237
Igor Mamaev <igor.mamaev1@gmail.com>
2026-01-08 15:25:53 +0300

    my N^3 solution

--- src/longest_palindrome.rs
+++ src/longest_palindrome.rs
@@ -1,22 +1,30 @@
 use std::collections::VecDeque;
 
 pub fn longest_palindrome(s: String) -> String {
-    let mut queue = VecDeque::from([s.clone()]);
-    let mut iter_count = 0;
-    let len = s.len();
-    let max_iter_count = (len / 2);
+    let mut queue = populate_queue(&s);
     while let Some(elem) = queue.pop_front() {
-        if elem.len() == 0 || is_palindrome(&elem) {
+        if elem.len() == 1 || is_palindrome(&elem) {
             return elem;
-        }
-        if iter_count < max_iter_count {
-            queue.push_back(s[..len - iter_count].to_string());
-            queue.push_back(s[iter_count..].to_string());
-            queue.push_back(s[iter_count..len - iter_count].to_string());
-            iter_count += 1;
         }
     }
     return "".to_string();
+}
+
+fn populate_queue(s: &String) -> VecDeque<String> {
+    let mut vec = Vec::new();
+    let len = s.len();
+    vec.push(s.clone());
+    for i in 0..s.len() {
+        for j in (i..=s.len()).rev() {
+            if i == j {
+                continue;
+            }
+            vec.push(s[i..j].to_string());
+        }
+    }
+    vec.sort_by_key(|x| x.len());
+    vec.reverse();
+    VecDeque::from(vec)
 }
 
 fn is_palindrome(s: &String) -> bool {
@@ -36,11 +44,22 @@ mod tests {
 
     #[test]
     fn test_longest_palindrome() {
-        let test_cases = [("babad", "bab"), ("cbbd", "bb")];
+        let test_cases = [
+            ("babad", "bab"),
+            ("cbbd", "bb"),
+            ("ac", "a"),
+            ("a", "a"),
+            ("abbcccba", "bcccb"),
+            ("abb", "bb"),
+        ];
 
         for (input, expected) in test_cases {
             let out = longest_palindrome(input.to_string());
-            assert_eq!(out.len(), expected.len());
+            assert_eq!(
+                out.len(),
+                expected.len(),
+                "out: \"{out}\", expected: \"{expected}\""
+            );
         }
     }
 }
